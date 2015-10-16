ffa73310f27ea0cea3b8f733203b3ea7b135054a
perfidiousscript <sam.moss321@gmail.com>
2015-10-15 20:17:47 -0500

    End of Day 1 Commit

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+function Unit(row,col,HP,AP){
+	this.row = row,
+	this.col = col,
+	this.HP = HP,
+	this.AP = AP
+}
+
+var player1 = new Unit(1,1,60,10);
+var enemy = new Unit(8,8,20,10);
+
+
 $(document).ready(function(){
 	$(".col").hover(function(){
 		$(this).toggleClass("hover")
